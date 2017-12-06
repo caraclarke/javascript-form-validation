9acdd4c39b69a3519831a7637f8632fcb5fdb867
caraclarke <cara.clarke8@gmail.com>
2017-12-06 10:36:51 -0500

    add password and confirm password field

---
+++ app/js/validation/password-validation.js
@@ -0,0 +1,15 @@
+// PW REQUIREMENTS
+const requirementsFields = document.querySelectorAll( ".js-req" );
+const passwordField = document.querySelectorAll( ".js-password" );
+const textSpChar = document.querySelectorAll( ".js-pwCharCri > .js-sp-char" );
+const reqRegex = /[<*%.]+/;
+
+// PW SPECIFIC DOM
+const pwChar = document.querySelectorAll( ".js-PwChar" );
+const pwCap = document.querySelectorAll( ".js-PwCap" );
+const pwLower = document.querySelectorAll( ".js-PwLower" );
+const pwNum = document.querySelectorAll( ".js-PwNum" );
+const textChar = document.querySelectorAll( ".js-char" );
+const textCap = document.querySelectorAll( ".js-cap" );
+const textLower = document.querySelectorAll( ".js-lower" );
+const textNum = document.querySelectorAll( ".js-num" );
