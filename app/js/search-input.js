22cde6536685500586ef11be8baed1b75d7fd90e
caraclarke <cara.clarke8@gmail.com>
2017-12-22 12:04:51 -0500

    add error handling for searching an empty field

--- app/js/search-input.js
+++ app/js/search-input.js
@@ -1,5 +1,6 @@
 const searchInput = document.querySelector( ".js-search-input" );
 const clearSearch = document.querySelector( ".js-search-clear" );
+const searchIcon = document.querySelector( ".js-search-enter" );
 
 searchInput.addEventListener( "keyup", ( e ) => {
   if ( searchInput.value !== "" ) {
@@ -14,4 +15,16 @@ clearSearch.addEventListener( "click", ( e ) => {
 
   searchInput.value = "";
   searchInput.parentElement.classList.remove( "has-text" );
+});
+
+searchIcon.addEventListener( "click", ( e ) => {
+  e.preventDefault();
+
+  if ( searchInput.value === "" ) {
+    // put the field in error
+    searchInput.setCustomValidity( "Please enter a valid search term" );
+    showErrorMessage( searchInput, "Please enter a valid search term" );
+  } else {
+    // call the search function
+  }
 });
